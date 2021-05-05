37f028c4ca65cbe7cf3586ad0bc3bd615bba273d
Jhunt <Jhunt646@west-mec.org>
2021-05-05 15:28:56 -0700

    Adding WIP navbar

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,16 +1,24 @@
 import React from 'react'
+import { BiMenu } from 'react-icons/bi'
 
 const Nav = () => {
+    // const [showing, setShowing] = useState(false)
+
     return (
-        <div className="Nav">
-            
+        <>
+        
+        <BiMenu className="hamburger" />
+        <div className="nav col-lg-3" >
+
             <ul>
                 <li>Home</li>
                 <li>Hour</li>
                 <li>Day</li>
                 <li>Week</li>
             </ul>
+
         </div>
+        </>
     )
 }
 
