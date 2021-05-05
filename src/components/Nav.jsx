bf3454d931497a4e58c4662f3e530edff9bdb14d
Jhunt <Jhunt646@west-mec.org>
2021-05-05 14:49:38 -0700

    testing if push works

---
+++ src/components/Nav.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Nav = () => {
+    return (
+        <div className="Nav">
+            
+            <ul>
+                <li>Home</li>
+                <li>Hour</li>
+                <li>Day</li>
+                <li>Week</li>
+            </ul>
+        </div>
+    )
+}
+
+export default Nav
