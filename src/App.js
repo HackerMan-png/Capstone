e4284198e01c378654cd869aa9fe3a7a9b0145a8
WRGcode <rg.willamreidgill@gmaill.com>
2021-05-12 14:41:23 -0700

    formatted weekly.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,16 @@
 import React, { useState } from 'react';
 import "./css/style.css"
 import '../node_modules/bootstrap/dist/css/bootstrap.css'
-import { Nav, Landing, Hourly, Daily, Today, Weekly } from './components/exports.jsx';
+import { Nav, Landing, Hourly, Today, Weekly } from './components/exports.jsx';
 import data from "./weather.json"
 
 function App() {
   const [landing, setLanding] = useState(true)
   const [hourly, setHourly] = useState(false)
-  const [daily, setDaily] = useState(false)
   const [today, setToday] = useState(false)
   const [weekly, setWeekly] = useState(false)
 
   const changePage = (page) => {
-      setDaily(false);
       setHourly(false);
       setLanding(false);
       setToday(false);
@@ -24,9 +22,6 @@ function App() {
           break;
         case "hourly":
           setHourly(true)
-          break;
-        case "daily":
-          setDaily(true)
           break;
         case "today":
           setToday(true)
@@ -43,7 +38,6 @@ function App() {
       <Nav changePage={changePage} />
       {landing && <Landing />}
       {hourly && <Hourly />}
-      {daily && <Daily />}
       {today && <Today />}
       {weekly && <Weekly />}
     </div>
