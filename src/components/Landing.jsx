5190f62eebf9cf2988d66f97b26cd763f5de0725
Jhunt <Jhunt646@west-mec.org>
2021-05-12 15:28:09 -0700

    Made landing page responsive

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -1,10 +1,25 @@
-import React from 'react';
-import { Container, Row, Col } from "react-bootstrap";
+import React, { useState } from 'react';
+// import { Container, Row, Col } from "react-bootstrap";
 import info from "../weather.json"
 
 const Landing = () => {
+
+  // const [hour, setHour] = useState(0)
+  // let interval;
+
+  let hour = new Date().getHours()
+  const arr = ['#040b3c', '#040b3c', '#192861', '#28166b', '#004372', '#016792', '#07729f', '#07729f', '#12a1c0', '#12a1c0', '#12a1c0', '#01C9D6', '#74c9e3', '#74c9e3', '#fd9e58', '#f18448', '#f06b7e', '#ca5a92', '#5b2c83', '#371a79', '#28166b', '#192861', '#040b3c', '#040b3c']
+  // function findHour() {
+  //   setHour(hour+1)
+  //   console.log(hour)
+  // }
+
+  // const colorChange= () => {
+  //     interval = setInterval(findHour, 1000)
+  // }
+
   return (
-    <div className="currentWrapper">
+    <div className="currentWrapper" style={{ background: arr[hour - 1] }}>
 
       <div id="cloud-intro">
 
@@ -13,6 +28,7 @@ const Landing = () => {
           <div className="temp">
             <p>{info.current.temp.toFixed(0)}°F</p>
           </div>
+
 
           <div className="location">
             <p>{info.timezone}</p>
