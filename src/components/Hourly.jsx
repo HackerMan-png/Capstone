fb166bc691f6a138275b61186c2f17497522832b
Daniel Chavez <dchave263@west-mec.org>
2021-05-17 15:28:24 -0700

    Done with Hourly. Added weather information. Just needs better styling.

--- src/components/Hourly.jsx
+++ src/components/Hourly.jsx
@@ -25,51 +25,110 @@ const Hourly = () => {
         <div id="hour1" class="box">
           <h1>{info.hourly[0].temp.toFixed(0)}°F</h1>
             <ul>
-              <li>Feels Like: {info.hourly[0].feels_like.toFixed(0)}°F</li>
+              <li></li><p>{info.hourly[0].weather[0].description}</p>
+              <li>Feels Like: </li><p>{info.hourly[0].feels_like.toFixed(0)}°F</p>
+              <li>Humidity: </li><p>{info.hourly[0].humidity.toFixed(0)}%</p>
+              <li>Wind Speed: </li><p>{info.hourly[0].wind_speed.toFixed(0)}mph</p>
             </ul>
         </div>
         <div id="hour2" class="box">
           <h1>{info.hourly[1].temp.toFixed(0)}°F</h1>
+            <ul>
+              <li></li><p>{info.hourly[1].weather[0].description}</p>
+              <li>Feels Like: </li><p>{info.hourly[1].feels_like.toFixed(0)}°F</p>
+              <li>Humidity: </li><p>{info.hourly[1].humidity.toFixed(0)}%</p>
+              <li>Wind Speed: </li><p>{info.hourly[1].wind_speed.toFixed(0)}mph</p>
+            </ul>
         </div>
         <div id="hour3" class="box">
           <h1>{info.hourly[2].temp.toFixed(0)}°F</h1>
-            <p></p>
+            <ul>
+              <li></li><p>{info.hourly[2].weather[0].description}</p>
+              <li>Feels Like: </li><p>{info.hourly[2].feels_like.toFixed(0)}°F</p>
+              <li>Humidity: </li><p>{info.hourly[2].humidity.toFixed(0)}%</p>
+              <li>Wind Speed: </li><p>{info.hourly[2].wind_speed.toFixed(0)}mph</p>
+            </ul>
         </div>
         <div id="hour4" class="box">
           <h1>{info.hourly[3].temp.toFixed(0)}°F</h1>
-            <p></p>
+            <ul>
+              <li></li><p>{info.hourly[3].weather[0].description}</p>
+              <li>Feels Like: </li><p>{info.hourly[3].feels_like.toFixed(0)}°F</p>
+              <li>Humidity: </li><p>{info.hourly[3].humidity.toFixed(0)}%</p>
+              <li>Wind Speed: </li><p>{info.hourly[3].wind_speed.toFixed(0)}mph</p>
+            </ul>
         </div>
         <div id="hour5" class="box">
           <h1>{info.hourly[4].temp.toFixed(0)}°F</h1>
-            <p></p>
+            <ul>
+              <li></li><p>{info.hourly[4].weather[0].description}</p>
+              <li>Feels Like: </li><p>{info.hourly[4].feels_like.toFixed(0)}°F</p>
+              <li>Humidity: </li><p>{info.hourly[4].humidity.toFixed(0)}%</p>
+              <li>Wind Speed: </li><p>{info.hourly[4].wind_speed.toFixed(0)}mph</p>
+            </ul>
         </div>
         <div id="hour6" class="box">
           <h1>{info.hourly[5].temp.toFixed(0)}°F</h1>
-            <p></p>
+            <ul>
+              <li></li><p>{info.hourly[5].weather[0].description}</p>
+              <li>Feels Like: </li><p>{info.hourly[5].feels_like.toFixed(0)}°F</p>
+              <li>Humidity: </li><p>{info.hourly[5].humidity.toFixed(0)}%</p>
+              <li>Wind Speed: </li><p>{info.hourly[5].wind_speed.toFixed(0)}mph</p>
+            </ul>
         </div>
         <div id="hour7" class="box">
           <h1>{info.hourly[6].temp.toFixed(0)}°F</h1>
-            <p></p>
+            <ul>
+              <li></li><p>{info.hourly[6].weather[0].description}</p>
+              <li>Feels Like: </li><p>{info.hourly[6].feels_like.toFixed(0)}°F</p>
+              <li>Humidity: </li><p>{info.hourly[6].humidity.toFixed(0)}%</p>
+              <li>Wind Speed: </li><p>{info.hourly[6].wind_speed.toFixed(0)}mph</p>
+            </ul>
         </div>
         <div id="hour8" class="box">
           <h1>{info.hourly[7].temp.toFixed(0)}°F</h1>
-            <p></p>
+            <ul>
+              <li></li><p>{info.hourly[7].weather[0].description}</p>
+              <li>Feels Like: </li><p>{info.hourly[7].feels_like.toFixed(0)}°F</p>
+              <li>Humidity: </li><p>{info.hourly[7].humidity.toFixed(0)}%</p>
+              <li>Wind Speed: </li><p>{info.hourly[7].wind_speed.toFixed(0)}mph</p>
+            </ul>
         </div>
         <div id="hour9" class="box">
           <h1>{info.hourly[8].temp.toFixed(0)}°F</h1>
-            <p></p>
+            <ul>
+              <li></li><p>{info.hourly[8].weather[0].description}</p>
+              <li>Feels Like: </li><p>{info.hourly[8].feels_like.toFixed(0)}°F</p>
+              <li>Humidity: </li><p>{info.hourly[8].humidity.toFixed(0)}%</p>
+              <li>Wind Speed: </li><p>{info.hourly[8].wind_speed.toFixed(0)}mph</p>
+            </ul>
         </div>
         <div id="hour10" class="box">
           <h1>{info.hourly[9].temp.toFixed(0)}°F</h1>
-            <p></p>
+            <ul>
+              <li></li><p>{info.hourly[9].weather[0].description}</p>
+              <li>Feels Like: </li><p>{info.hourly[9].feels_like.toFixed(0)}°F</p>
+              <li>Humidity: </li><p>{info.hourly[9].humidity.toFixed(0)}%</p>
+              <li>Wind Speed: </li><p>{info.hourly[9].wind_speed.toFixed(0)}mph</p>
+            </ul>
         </div>
         <div id="hour11" class="box">
           <h1>{info.hourly[10].temp.toFixed(0)}°F</h1>
-            <p></p>
+            <ul>
+              <li></li><p>{info.hourly[10].weather[0].description}</p>
+              <li>Feels Like: </li><p>{info.hourly[10].feels_like.toFixed(0)}°F</p>
+              <li>Humidity: </li><p>{info.hourly[10].humidity.toFixed(0)}%</p>
+              <li>Wind Speed: </li><p>{info.hourly[10].wind_speed.toFixed(0)}mph</p>
+            </ul>
         </div>
         <div id="hour12" class="box">
           <h1>{info.hourly[11].temp.toFixed(0)}°F</h1>
-            <p></p>
+            <ul>
+              <li></li><p>{info.hourly[11].weather[0].description}</p>
+              <li>Feels Like: </li><p>{info.hourly[11].feels_like.toFixed(0)}°F</p>
+              <li>Humidity: </li><p>{info.hourly[11].humidity.toFixed(0)}%</p>
+              <li>Wind Speed: </li><p>{info.hourly[11].wind_speed.toFixed(0)}mph</p>
+            </ul>
         </div>
 
       </div>
