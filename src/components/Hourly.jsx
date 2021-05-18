219c438f120b406af4257c850f8c145d66de88b8
Daniel Chavez <dchave263@west-mec.org>
2021-05-18 15:28:00 -0700

    Added hour, changed font, fixed div style for hour and icon.

--- src/components/Hourly.jsx
+++ src/components/Hourly.jsx
@@ -1,5 +1,7 @@
-import React from 'react'
-import info from "../weather.json"
+import React from 'react';
+import { icons } from 'react-icons/lib';
+import info from "../weather.json";
+import clearDay from './icons/clearDay.png';
 
 const Hourly = () => {
 
@@ -24,8 +26,11 @@ const Hourly = () => {
 
         <div id="hour1" class="box">
           <h1>{info.hourly[0].temp.toFixed(0)}°F</h1>
+
+          <img src={clearDay.clearDay} alt="" srcset="" />
+          
+          <p className="Hour">1:00am</p>
             <ul>
-              <li></li><p>{info.hourly[0].weather[0].description}</p>
               <li>Feels Like: </li><p>{info.hourly[0].feels_like.toFixed(0)}°F</p>
               <li>Humidity: </li><p>{info.hourly[0].humidity.toFixed(0)}%</p>
               <li>Wind Speed: </li><p>{info.hourly[0].wind_speed.toFixed(0)}mph</p>
@@ -34,7 +39,6 @@ const Hourly = () => {
         <div id="hour2" class="box">
           <h1>{info.hourly[1].temp.toFixed(0)}°F</h1>
             <ul>
-              <li></li><p>{info.hourly[1].weather[0].description}</p>
               <li>Feels Like: </li><p>{info.hourly[1].feels_like.toFixed(0)}°F</p>
               <li>Humidity: </li><p>{info.hourly[1].humidity.toFixed(0)}%</p>
               <li>Wind Speed: </li><p>{info.hourly[1].wind_speed.toFixed(0)}mph</p>
@@ -42,8 +46,8 @@ const Hourly = () => {
         </div>
         <div id="hour3" class="box">
           <h1>{info.hourly[2].temp.toFixed(0)}°F</h1>
+          <p className="Hour"></p>
             <ul>
-              <li></li><p>{info.hourly[2].weather[0].description}</p>
               <li>Feels Like: </li><p>{info.hourly[2].feels_like.toFixed(0)}°F</p>
               <li>Humidity: </li><p>{info.hourly[2].humidity.toFixed(0)}%</p>
               <li>Wind Speed: </li><p>{info.hourly[2].wind_speed.toFixed(0)}mph</p>
@@ -52,7 +56,6 @@ const Hourly = () => {
         <div id="hour4" class="box">
           <h1>{info.hourly[3].temp.toFixed(0)}°F</h1>
             <ul>
-              <li></li><p>{info.hourly[3].weather[0].description}</p>
               <li>Feels Like: </li><p>{info.hourly[3].feels_like.toFixed(0)}°F</p>
               <li>Humidity: </li><p>{info.hourly[3].humidity.toFixed(0)}%</p>
               <li>Wind Speed: </li><p>{info.hourly[3].wind_speed.toFixed(0)}mph</p>
@@ -61,7 +64,6 @@ const Hourly = () => {
         <div id="hour5" class="box">
           <h1>{info.hourly[4].temp.toFixed(0)}°F</h1>
             <ul>
-              <li></li><p>{info.hourly[4].weather[0].description}</p>
               <li>Feels Like: </li><p>{info.hourly[4].feels_like.toFixed(0)}°F</p>
               <li>Humidity: </li><p>{info.hourly[4].humidity.toFixed(0)}%</p>
               <li>Wind Speed: </li><p>{info.hourly[4].wind_speed.toFixed(0)}mph</p>
@@ -70,7 +72,6 @@ const Hourly = () => {
         <div id="hour6" class="box">
           <h1>{info.hourly[5].temp.toFixed(0)}°F</h1>
             <ul>
-              <li></li><p>{info.hourly[5].weather[0].description}</p>
               <li>Feels Like: </li><p>{info.hourly[5].feels_like.toFixed(0)}°F</p>
               <li>Humidity: </li><p>{info.hourly[5].humidity.toFixed(0)}%</p>
               <li>Wind Speed: </li><p>{info.hourly[5].wind_speed.toFixed(0)}mph</p>
@@ -79,7 +80,6 @@ const Hourly = () => {
         <div id="hour7" class="box">
           <h1>{info.hourly[6].temp.toFixed(0)}°F</h1>
             <ul>
-              <li></li><p>{info.hourly[6].weather[0].description}</p>
               <li>Feels Like: </li><p>{info.hourly[6].feels_like.toFixed(0)}°F</p>
               <li>Humidity: </li><p>{info.hourly[6].humidity.toFixed(0)}%</p>
               <li>Wind Speed: </li><p>{info.hourly[6].wind_speed.toFixed(0)}mph</p>
@@ -88,7 +88,6 @@ const Hourly = () => {
         <div id="hour8" class="box">
           <h1>{info.hourly[7].temp.toFixed(0)}°F</h1>
             <ul>
-              <li></li><p>{info.hourly[7].weather[0].description}</p>
               <li>Feels Like: </li><p>{info.hourly[7].feels_like.toFixed(0)}°F</p>
               <li>Humidity: </li><p>{info.hourly[7].humidity.toFixed(0)}%</p>
               <li>Wind Speed: </li><p>{info.hourly[7].wind_speed.toFixed(0)}mph</p>
@@ -97,7 +96,6 @@ const Hourly = () => {
         <div id="hour9" class="box">
           <h1>{info.hourly[8].temp.toFixed(0)}°F</h1>
             <ul>
-              <li></li><p>{info.hourly[8].weather[0].description}</p>
               <li>Feels Like: </li><p>{info.hourly[8].feels_like.toFixed(0)}°F</p>
               <li>Humidity: </li><p>{info.hourly[8].humidity.toFixed(0)}%</p>
               <li>Wind Speed: </li><p>{info.hourly[8].wind_speed.toFixed(0)}mph</p>
@@ -106,7 +104,6 @@ const Hourly = () => {
         <div id="hour10" class="box">
           <h1>{info.hourly[9].temp.toFixed(0)}°F</h1>
             <ul>
-              <li></li><p>{info.hourly[9].weather[0].description}</p>
               <li>Feels Like: </li><p>{info.hourly[9].feels_like.toFixed(0)}°F</p>
               <li>Humidity: </li><p>{info.hourly[9].humidity.toFixed(0)}%</p>
               <li>Wind Speed: </li><p>{info.hourly[9].wind_speed.toFixed(0)}mph</p>
@@ -115,7 +112,6 @@ const Hourly = () => {
         <div id="hour11" class="box">
           <h1>{info.hourly[10].temp.toFixed(0)}°F</h1>
             <ul>
-              <li></li><p>{info.hourly[10].weather[0].description}</p>
               <li>Feels Like: </li><p>{info.hourly[10].feels_like.toFixed(0)}°F</p>
               <li>Humidity: </li><p>{info.hourly[10].humidity.toFixed(0)}%</p>
               <li>Wind Speed: </li><p>{info.hourly[10].wind_speed.toFixed(0)}mph</p>
@@ -124,7 +120,6 @@ const Hourly = () => {
         <div id="hour12" class="box">
           <h1>{info.hourly[11].temp.toFixed(0)}°F</h1>
             <ul>
-              <li></li><p>{info.hourly[11].weather[0].description}</p>
               <li>Feels Like: </li><p>{info.hourly[11].feels_like.toFixed(0)}°F</p>
               <li>Humidity: </li><p>{info.hourly[11].humidity.toFixed(0)}%</p>
               <li>Wind Speed: </li><p>{info.hourly[11].wind_speed.toFixed(0)}mph</p>
